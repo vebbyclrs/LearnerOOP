50824310bf3a7287bcb5902bcacf0acfccb21c8c
Vebby Clarissa <vebbyclarissa@gmail.com>
2019-05-15 12:16:54 +0700

    Initial Commit

---
+++ LearnerOOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnerOOP
+//
+//  Created by Vebby Clarissa on 15/05/19.
+//  Copyright © 2019 Vebby Clarissa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
